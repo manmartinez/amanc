e903a20ff8a51773d37fe4bb0e4cb4a342f29e15
Manuel Martinez <vmmf08@gmail.com>
2013-02-09 17:45:09 -0600

    Rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amanc::Application.config.secret_token = 'bc0863a72d5f20456e722787696c5d371384918e6f87623187160dcf3e1c9bd1afbd08bdd8f0d4d5abc8bf1178b8139ebda80faa5f46b1e622205003b645b4e8'
